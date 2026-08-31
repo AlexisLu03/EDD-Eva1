04f0e205e28cd958f2172c71a52c9bf2e550720f
AlexisLu03 <L25550709@CHIHUAHUA2.TECNM.MX>
2026-08-31 16:53:41 -0600

    Add files via upload

---
+++ EstrucDat/src/main/java/com/mycompany/estrucdat/Eva1_1_SCOPE.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estrucdat;
+
+/**
+ *
+ * @author dell
+ */
+public class Eva1_1_SCOPE {
+    public static void main(String[] args) {
+        int x = 100;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = "+ i);
+            x++;
+        }
+        System.out.println("i= "+i);
+        System.out.println("x= "+x);
+    }
+    
+    public static void OtraFuncion() {
+        System.out.println("X = "+ x);
+    }
+}
