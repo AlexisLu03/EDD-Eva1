74758a1e940f13130154ea1aec5fcb3b1f076584
AlexisLu03 <L25550709@CHIHUAHUA2.TECNM.MX>
2026-09-07 22:01:40 -0600

    Add files via upload

---
+++ Eva1_4_Objetos/src/main/java/com/mycompany/eva1_4_objetos/Eva1_4_Objetos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.eva1_4_objetos;
+
+/**
+ *
+ * @author dell
+ */
+public class Eva1_4_Objetos {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println(prueba);
+        //Eliminar prueba;
+        // Terminar programa -- Garbaje collector
+        // Eliminar directamente el objeto
+    }
+
+    class Prueba {}
+    ;
+}
